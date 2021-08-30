320e45d33a39d06c15e32d158fb97194dcfa1277
ianbelfatto <ibelfatto@gmail.com>
2021-08-30 12:02:49 -0400

    attempts to fix routing

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 http.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port *: 3000");
 });
