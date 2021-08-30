a89405d248f94593129277cedd35b77b6cb17868
ianbelfatto <ibelfatto@gmail.com>
2021-08-30 11:46:32 -0400

    edits listen port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(5000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port *: 3000");
 });
 
